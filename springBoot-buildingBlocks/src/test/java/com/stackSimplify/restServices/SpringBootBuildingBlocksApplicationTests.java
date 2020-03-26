b5d9fbf9231d7e9680841e1612327ea4ab4466da
siddharthnew <siddharthtrivedi3@gmail.com>
2020-03-26 18:18:55 +0530

    First Commit

---
+++ springBoot-buildingBlocks/src/test/java/com/stackSimplify/restServices/SpringBootBuildingBlocksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stackSimplify.restServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBuildingBlocksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
